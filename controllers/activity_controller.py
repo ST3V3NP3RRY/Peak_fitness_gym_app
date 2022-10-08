04ff62a51a1f027832bbd3ea78d8792f00dd4e28
Steven perry <Sperry3006@gmail.com>
2022-10-08 08:51:00 +0100

    Added form to app for adding new classes

--- controllers/activity_controller.py
+++ controllers/activity_controller.py
@@ -10,3 +10,18 @@ activity_blueprint = Blueprint("activity", __name__)
 def activity():
     activities = activity_repository.select_all()
     return render_template("activities/index.html", activities=activities)
+
+
+@activity_blueprint.route("/activities/new", methods=["GET"])
+def new_activity():
+    return render_template("/activities/new.html")
+
+
+# make a new exercise activity
+@activity_blueprint.route("/activities/index", methods=["POST"])
+def create_activity():
+    name = request.form["name"]
+    title = request.form["title"]
+    activity = Activity(name, title)
+    activity_repository.save(activity)
+    return redirect("/activities/index")
